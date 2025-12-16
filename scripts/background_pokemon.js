73570a37bc85ab44c89553bffd85699d90a74166
Ties van Zandbrink <teezee726@gmail.com>
2025-12-16 17:12:37 +0100

    Add past paradox mons base SV

--- scripts/background_pokemon.js
+++ scripts/background_pokemon.js
@@ -179,6 +179,8 @@ pokemon = [{
 }, {
     "bronzor": ["bronzor"]
 }, {
+    "brutebonnet": ["brutebonnet"]
+}, {
     "bruxish": ["bruxish"]
 }, {
     "budew": ["budew"]
@@ -587,6 +589,8 @@ pokemon = [{
 }, {
     "florges": ["florges", "florges-blue", "florges-orange", "florges-white", "florges-yellow"]
 }, {
+    "fluttermane": ["fluttermane"]
+}, {
     "flygon": ["flygon"]
 }, {
     "fomantis": ["fomantis"]
@@ -708,6 +712,8 @@ pokemon = [{
     "grapploct": ["grapploct"]
 }, {
     "graveler": ["graveler", "graveler-alola"]
+}, {
+    "greattusk": ["greattusk"]
 }, {
     "greavard": ["greavard"]
 }, {
@@ -890,6 +896,8 @@ pokemon = [{
     "komala": ["komala"]
 }, {
     "kommo-o": ["kommo-o"]
+}, {
+    "koraidon": ["koraidon"]
 }, {
     "krabby": ["krabby"]
 }, {
@@ -1451,6 +1459,8 @@ pokemon = [{
 }, {
     "sandygast": ["sandygast"]
 }, {
+    "sandyshocks": ["sandyshocks"]
+}, {
     "sawk": ["sawk"]
 }, {
     "sawsbuck": ["sawsbuck", "sawsbuck-autumn", "sawsbuck-summer", "sawsbuck-winter"]
@@ -1470,6 +1480,8 @@ pokemon = [{
     "scrafty": ["scrafty"]
 }, {
     "scraggy": ["scraggy"]
+}, {
+    "screamtail": ["screamtail"]
 }, {
     "scyther": ["scyther", "scyther-f"]
 }, {
@@ -1570,6 +1582,8 @@ pokemon = [{
     "slakoth": ["slakoth"]
 }, {
     "sliggoo": ["sliggoo", "sliggoo-hisui"]
+}, {
+    "slitherwing": ["slitherwing"]
 }, {
     "slowbro": ["slowbro", "slowbro-galar", "slowbro-mega"]
 }, {
@@ -4837,6 +4851,7 @@ floating_pokemon = [
     "chimecho",
     "chingling",
     "claydol",
+    "clefable-mega",
     "cloyster",
     "cofagrigus",
     "combee",
@@ -5040,7 +5055,6 @@ floating_pokemon = [
     "staravia",
     "staravia-f",
     "starmie",
-    "starmie-mega",
     "staryu",
     "stratagem",
     "swablu",
